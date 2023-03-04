618f30f424d43761c653f35c762bbaf1f8ba01bd
vladislav.kori <vladislav.kori.work@gmail.com>
2023-03-04 21:35:43 +0500

    [init]

---
+++ scripts/background.js
@@ -0,0 +1,23 @@
+var config = {
+    mode: "fixed_servers",
+    rules: {
+        proxyForHttps: {
+            scheme: "socks5",
+            host: "158.69.225.110",
+            port: 59166,
+            scheme: "socks5"
+        },
+        bypassList: ['*youtube.com']
+    }
+};
+
+chrome.proxy.settings.set(
+    {value: config, scope: 'regular'},
+    function() {
+        console.log("work")
+    }
+);
+
+chrome.proxy.onProxyError.addListener(details => {
+    if (details.fatal) {console.log('error', details)}
+})
