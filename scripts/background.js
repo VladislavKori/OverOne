79a5187a2651a7bf7181176767fabd2dabe12bd0
vladislav.kori <vladislav.kori.work@gmail.com>
2023-04-07 03:23:19 +0500

    update

--- scripts/background.js
+++ scripts/background.js
@@ -1,18 +1,24 @@
-var config = {
-    mode: "fixed_servers",
-    rules: {
-        proxyForHttps: {
-            scheme: "socks5",
-            host: "158.69.225.110",
-            port: 59166,
-            scheme: "socks5"
-        },
-        bypassList: ['*youtube.com']
-    }
-};
+const generateConfig = () => {
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+            proxyForHttps: {
+                scheme: "socks5",
+                host: "158.69.225.110",
+                port: 59166,
+                scheme: "socks5"
+            },
+            bypassList: [
+                "*.radiorecord.ru", // work pattern
+                "*.youtube.com"
+            ]
+        }
+    };
+    return config;
+}
 
 chrome.proxy.settings.set({
-        value: config,
+        value: generateConfig(),
         scope: 'regular'
     },
     function () {
@@ -22,6 +28,7 @@ chrome.proxy.settings.set({
 
 chrome.proxy.onProxyError.addListener(details => {
     if (details.fatal) {
+        // console.log('err') // for testing
         chrome.proxy.settings.clear({},
             function () {
                 console.log('chrome settings clear')
