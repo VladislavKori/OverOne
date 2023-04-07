5e9c00269fedf035410c56579782bf3587502d5e
vladislav.kori <vladislav.kori.work@gmail.com>
2023-04-07 19:49:12 +0500

    add list with proxys

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,24 @@
+import {data} from './data.js'
+const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
 const generateConfig = () => {
+
+    const rNum = random(0, data.length - 1);
+    const connection = data[rNum]
+    
+    const host = connection.ip;
+    const scheme = connection.protocols[0];
+    const port = Number(connection.port);
+
+    console.log(host, scheme, port)
+
     var config = {
         mode: "fixed_servers",
         rules: {
             proxyForHttps: {
-                scheme: "socks5",
-                host: "158.69.225.110",
-                port: 59166,
-                scheme: "socks5"
+                scheme: scheme,
+                host: host,
+                port: port,
             },
             bypassList: [
                 "*.radiorecord.ru", // work pattern
@@ -14,6 +26,8 @@ const generateConfig = () => {
             ]
         }
     };
+
+    console.log(config)
     return config;
 }
 
@@ -22,13 +36,12 @@ chrome.proxy.settings.set({
         scope: 'regular'
     },
     function () {
-        console.log("work")
+        // console.log(data)
     }
 );
 
 chrome.proxy.onProxyError.addListener(details => {
     if (details.fatal) {
-        // console.log('err') // for testing
         chrome.proxy.settings.clear({},
             function () {
                 console.log('chrome settings clear')
