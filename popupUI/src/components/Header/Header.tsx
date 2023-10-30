1c54b482fab23a0837ad6c2c8df2d82440148214
vladislavkori <vladislav.kori.work@gmail.com>
2023-10-31 02:56:37 +0500

    update build options

--- popupUI/src/components/Header/Header.tsx
+++ popupUI/src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header class="header">
             <div class="logo">
-                <a class="logo__link" href="/">
+                <a class="logo__link" target="_blank" href="https://github.com/VladislavKori/OverOne">
                     <img src={Logo} />
                 </a>
             </div>
